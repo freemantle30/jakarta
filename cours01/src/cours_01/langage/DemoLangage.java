bbfa57286c19623e03a9d4673a877ce2860cd53b
eliot <eliott_vincent@bluewin.ch>
2025-02-20 18:21:28 +0100

    initial commit

---
+++ cours01/src/cours_01/langage/DemoLangage.java
@@ -0,0 +1,16 @@
+package cours_01.langage;
+
+public class DemoLangage {
+    public static void main(String[] args) {
+        int entier = 10;
+        char caractre = 'a';
+
+        System.out.println("Hello World");
+        if (entier > 12) {
+            System.out.println(entier);
+        }
+        else{
+            System.out.println("HOLA");
+        }
+    }
+}
